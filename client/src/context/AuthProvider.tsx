0aec00e0100856cb047af7ff67a8c424de513364
MuhannadQ <muhannad.y.q@gmail.com>
2024-01-18 18:46:43 +0200

    Client: Add Cognito Authentication

--- client/src/context/AuthProvider.tsx
+++ client/src/context/AuthProvider.tsx
@@ -1,18 +1,27 @@
 import { createContext, useState, useEffect } from 'react'
+import { cognito, CognitoLoginRes, CognitoErr, getErrorMessage } from '@/lib/cognito'
 
 interface User {
-  id: string
+  id?: string
   username: string
+  accessToken: string
   email?: string
 }
 
 interface AuthContextProps {
   isLoading: boolean
   user: User | null
+  error: string | undefined
+  newPasswordRequired: boolean
   isAuthenticated: boolean
   login: (username: string, password: string) => Promise<void>
+  changePassword: (newPassword: string) => Promise<void>
   logout: () => void
 }
+
+const AuthDataKey = 'auth-data'
+
+type AuthData = NonNullable<NonNullable<CognitoLoginRes>['AuthenticationResult']>
 
 export const AuthContext = createContext<AuthContextProps | undefined>(undefined)
 
@@ -22,18 +31,44 @@ interface AuthProviderProps {
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<User | null>(null)
+  const [newPasswordRequired, setNewPasswordRequired] = useState(false)
+  const [error, setError] = useState<string | undefined>(undefined)
   const [isLoading, setIsLoading] = useState(true)
+
+  const updateState = (res: CognitoLoginRes, err: CognitoErr, username: string) => {
+    const errorMessage = getErrorMessage(err)
+    setError(errorMessage)
+
+    if (res) {
+      sessionStorage.setItem(AuthDataKey, JSON.stringify(res.AuthenticationResult))
+      setUser({ username, accessToken: res.AuthenticationResult?.AccessToken as string })
+    } else {
+      setUser(null)
+    }
+
+    setIsLoading(false)
+  }
 
   const login = async (username: string, password: string) => {
     setIsLoading(true)
-    await new Promise((resolve) => setTimeout(resolve, 1500))
-    setIsLoading(false)
-    const authenticatedUser: User = { id: '1', username }
-    setUser(authenticatedUser)
+    const { challenge, res, err } = await cognito.authenticate(username, password)
+    console.log({ res, err })
+    setNewPasswordRequired(Boolean(challenge))
+    updateState(res, err, username)
   }
 
-  const logout = () => {
-    // clear session or token
+  const changePassword = async (newPassword: string) => {
+    setIsLoading(true)
+    const { res, err, username } =
+      await cognito.respondToNewPasswordChallenge(newPassword)
+    console.log({ res, err })
+    updateState(res, err, username)
+  }
+
+  const logout = async () => {
+    if (!user) throw new Error('Unexpected behavior. User must be set.')
+    sessionStorage.removeItem(AuthDataKey)
+    await cognito.signOut(user.accessToken)
     setUser(null)
   }
 
@@ -41,10 +76,14 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     // check token is valid
     const authenticate = async () => {
       setIsLoading(true)
-      await new Promise((resolve) => setTimeout(resolve, 3000))
-      const authenticated = true /* TODO: perform authentication check */
-      if (authenticated) {
-        setUser({ id: '1', username: 'stored_user', email: 'stored_user@example.com' })
+      const storedAuthData = sessionStorage.getItem(AuthDataKey)
+
+      if (storedAuthData) {
+        const accessToken = (JSON.parse(storedAuthData) as AuthData).AccessToken as string
+        const { user } = await cognito.getUser(accessToken)
+        // when token expires getUser returns "NotAuthorizedException" error
+        // setTimeout(() => setUser(user ? { ...user, accessToken } : null), 3000)
+        setUser(user ? { ...user, accessToken } : null)
       }
       setIsLoading(false)
     }
@@ -55,7 +94,10 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     isLoading,
     user,
     isAuthenticated: user !== null,
+    error,
+    newPasswordRequired,
     login,
+    changePassword,
     logout,
   }
 
---
+++ client/src/lib/cognito.ts
@@ -0,0 +1,166 @@
+import {
+  CognitoIdentityProviderClient,
+  CognitoIdentityProviderServiceException,
+  InitiateAuthCommand,
+  GlobalSignOutCommand,
+  RespondToAuthChallengeCommand,
+  InitiateAuthResponse,
+  GlobalSignOutResponse,
+  RespondToAuthChallengeResponse,
+  ChallengeNameType,
+  AuthenticationResultType,
+  GetUserCommand,
+  GetUserResponse,
+} from '@aws-sdk/client-cognito-identity-provider'
+
+const AWS_REGION = import.meta.env.VITE_AWS_REGION as string
+
+// import.meta.env.VITE_AWS_COGNITO_USER_POOL_ID
+
+const AWS_COGNITO_USER_POOL_CLIENT_ID = import.meta.env
+  .VITE_AWS_COGNITO_USER_POOL_CLIENT_ID as string
+
+export type CognitoLoginRes = { AuthenticationResult?: AuthenticationResultType } | null
+export type CognitoErr = string | null
+
+type ChallengeData = {
+  session: string
+  params: { username: string }
+}
+
+class CognitoClient {
+  private client: CognitoIdentityProviderClient
+  private challenge: ChallengeData | undefined
+
+  constructor() {
+    this.client = new CognitoIdentityProviderClient({
+      region: AWS_REGION,
+    })
+  }
+
+  async authenticate(username: string, password: string) {
+    let res: InitiateAuthResponse | null = null
+    let err: string | null = null
+
+    const command = new InitiateAuthCommand({
+      ClientId: AWS_COGNITO_USER_POOL_CLIENT_ID,
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      AuthParameters: { USERNAME: username, PASSWORD: password },
+    })
+
+    try {
+      res = await this.client.send(command)
+    } catch (error) {
+      err = handleCognitoError(error)
+    }
+
+    if (res !== null && res.ChallengeName) {
+      this.challenge = {
+        session: res.Session as string,
+        params: {
+          username: res.ChallengeParameters?.USER_ID_FOR_SRP as string,
+        },
+      }
+      res = null
+    }
+
+    return { res, err, challenge: this.challenge }
+  }
+
+  async respondToNewPasswordChallenge(newPassword: string) {
+    if (!this.challenge)
+      throw new Error(
+        'Cannot call "respondToNewPasswordChallenge" when not in "challenge" state'
+      )
+
+    let res: RespondToAuthChallengeResponse | null = null
+    let err: string | null = null
+    const username = this.challenge.params.username
+
+    const command = new RespondToAuthChallengeCommand({
+      ClientId: AWS_COGNITO_USER_POOL_CLIENT_ID,
+      ChallengeName: ChallengeNameType.NEW_PASSWORD_REQUIRED,
+      ChallengeResponses: {
+        USERNAME: username,
+        NEW_PASSWORD: newPassword,
+      },
+      Session: this.challenge.session,
+    })
+
+    try {
+      res = await this.client.send(command)
+      this.challenge = undefined
+    } catch (error) {
+      err = handleCognitoError(error)
+    }
+
+    return { res, err, username }
+  }
+
+  async getUser(accessToken?: string) {
+    const command = new GetUserCommand({
+      AccessToken: accessToken,
+    })
+
+    let res: GetUserResponse | null = null
+    let err: string | null = null
+
+    try {
+      res = await this.client.send(command)
+    } catch (error) {
+      err = handleCognitoError(error)
+    }
+    const user = res?.Username ? { username: res.Username } : null
+    return { user, err }
+  }
+
+  async signOut(accessToken?: string) {
+    const command = new GlobalSignOutCommand({
+      AccessToken: accessToken,
+    })
+
+    let res: GlobalSignOutResponse | null = null
+    let err: string | null = null
+
+    try {
+      res = await this.client.send(command)
+    } catch (error) {
+      err = handleCognitoError(error)
+    }
+    return { res, err }
+  }
+}
+
+export const cognito = new CognitoClient()
+
+// https://medium.com/@adi2308/aws-cognito-with-reactjs-for-authentication-c8916b873ccb
+
+type SupportedCognitoExceptions =
+  | 'NotAuthorizedException' // happens on login or when using expired token
+  | 'UserNotFoundException'
+  | 'InvalidParameterException'
+  | 'InvalidPasswordException'
+
+const CognitoErrorCodeToMessage: Record<string, string> = {
+  NotAuthorizedException: 'Incorrect username or password',
+  UserNotFoundException: 'User does not exist',
+  InvalidParameterException: 'Invalid username or password',
+  InvalidPasswordException:
+    'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one number',
+} satisfies Record<SupportedCognitoExceptions, string>
+
+export function getErrorMessage(err: CognitoErr) {
+  return err
+    ? CognitoErrorCodeToMessage[err] ?? 'Unrecognized error. Check logs.'
+    : undefined
+}
+
+function handleCognitoError(error: unknown) {
+  if (error instanceof CognitoIdentityProviderServiceException) {
+    console.error('Cognito Error:', error)
+    return error.name
+  } else {
+    console.error(error)
+    return 'Unknown Error'
+  }
+}
